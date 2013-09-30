c76e5839cf453a72756021aae16afda359f0e0eb
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-30 10:30:48 -0500

    got form to appear and disappear as desired

--- comments.js
+++ comments.js
@@ -1,12 +1,19 @@
-var form = "<form id='new_comment' placeholder='add your comment here'><textarea></textarea><input id='authorName' type='text' value='authorName'></input><input id='submit_comment' type='submit' name='submit_comment'></input></form>";
-
+var form = "<form id='new_comment'><textarea name='new_comment' placeholder='add your comment here'></textarea><input id='authorName' type='text' placeholder='author name here'></input><button type='submit' id='submit_comment' name='submit_comment'>submit</button></form>";
+var button = "<button id='new_comment_button'>New Comment</button>";
 // Shorthand for document ready
 $(function(){
   
-  $('#new_comment_button').on('click', function(e) {
-    $('#new_comment_button').hide();
+  $('#comment_area').on('click', '#new_comment_button', function(e) {
+    e.preventDefault();
+    $('#new_comment_button').remove();
     $('#comment_area').append(form);
+  });
 
+  $('#comment_area').on('submit', '#new_comment', function(e) {
+    e.preventDefault();
+    $('#new_comment').remove();
+    $('#comment_list').append('<li>Comment Stuff</li>');
+    $('#comment_area').append(button);
   });
 
 });
