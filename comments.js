bc12b96eb37473ce354a4adfb971fc39f340bc01
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-30 10:53:59 -0500

    working

--- comments.js
+++ comments.js
@@ -1,5 +1,6 @@
-var form = "<form id='new_comment'><textarea name='new_comment' placeholder='add your comment here'></textarea><input id='authorName' type='text' placeholder='author name here'></input><button type='submit' id='submit_comment' name='submit_comment'>submit</button></form>";
+var form = "<form id='new_comment'><textarea id='new_comment_text' name='new_comment_text' placeholder='add your comment here'></textarea><input id='authorName' type='text' placeholder='author name here'></input><button type='submit' id='submit_comment' name='submit_comment'>submit</button></form>";
 var button = "<button id='new_comment_button'>New Comment</button>";
+
 // Shorthand for document ready
 $(function(){
   
@@ -9,10 +10,14 @@ $(function(){
     $('#comment_area').append(form);
   });
 
+
   $('#comment_area').on('submit', '#new_comment', function(e) {
     e.preventDefault();
+    var comment = $('#new_comment_text').val();
+    var author = $('#authorName').val();
+    console.log(comment);
     $('#new_comment').remove();
-    $('#comment_list').append('<li>Comment Stuff</li>');
+    $('#comment_list').append("<li>" + comment + "<span class='author'>" + author + "</span></li>");
     $('#comment_area').append(button);
   });
 
