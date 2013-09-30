5ab6cd57abae69806e4af5c948e659d578c4e343
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-30 09:35:22 -0500

    new comment form working

--- comments.js
+++ comments.js
@@ -1,4 +1,12 @@
+var form = "<form id='new_comment' placeholder='add your comment here'><textarea></textarea><input id='authorName' type='text' value='authorName'></input><input id='submit_comment' type='submit' name='submit_comment'></input></form>";
+
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+  
+  $('#new_comment_button').on('click', function(e) {
+    $('#new_comment_button').hide();
+    $('#comment_area').append(form);
+
+  });
+
 });
